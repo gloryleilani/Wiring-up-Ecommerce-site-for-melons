191b76477f66018744ac7d648d6c1be10c66c055
Glory Ludwick <lani.ludwick@gmail.com>
2020-07-24 08:01:54 +0000

    Wiring up an Ecommerce site selling melons

---
+++ customers.py
@@ -0,0 +1,54 @@
+"""Customers at Hackbright."""
+
+
+class Customer(object):
+    """Ubermelon customer."""
+
+    def __init__(self,
+                 first_name,
+                 last_name,
+                 email,
+                 password,
+                 ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+
+    def __repr__(self):
+        """Convenience method to show information about melon in console."""
+
+        return "<Customer: {}, {}, {}>".format(self.first_name, self.last_name, 
+            self.email, self.password)
+
+
+def read_customers_from_file(filepath):
+    """Read customer data and populate dictionary of customers.
+
+    Dictionary will be {id: email}
+    """
+
+    customers = {}
+
+    with open(filepath) as file:
+        for line in file:
+            (first_name,
+            last_name,
+            email,
+            password) = line.strip().split("|")
+
+            customers[email] = Customer(first_name, last_name, email, password)
+
+    return customers
+
+
+def get_by_email(email):
+    """Return a customer, given their email."""
+
+    # This relies on access to the global dictionary `customers`
+
+    return customers[email]
+
+
+customers = read_customers_from_file("customers.txt")
